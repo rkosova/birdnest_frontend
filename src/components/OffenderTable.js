cfbf5058dc6bb58ec5c56ff7f760c508a8d49b62
Ron Kosova <ron.kosova@gmail.com>
2023-01-18 14:45:43 +0200

    Added OffenderTable component

---
+++ src/components/OffenderTable.js
@@ -0,0 +1,37 @@
+import { useEffect } from "react"; 
+
+import OffenderRow from "./OffenderRow";
+
+
+const OffenderTable = ({ offenders }) => {
+
+    return (
+        <table className='offenderTable'>
+            <thead>
+            <tr>
+                <th></th>
+                <th>Snapshot</th>
+                <th>Distance</th>
+                <th>Pilot ID</th>
+                <th>Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+            </tr>  
+            </thead>
+            <tbody>
+            { offenders.map((offender, i) => {
+                return (
+                <OffenderRow 
+                    offender={ offender } 
+                    index = { i } 
+                    key={ offender.id }
+                />
+                );
+            }) }
+            </tbody>
+      </table>
+    );
+}
+
+export default OffenderTable;
