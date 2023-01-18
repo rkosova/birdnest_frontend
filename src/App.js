e7eda83f2a95892f6c074232eee722fa45b1262d
Ron Kosova <ron.kosova@gmail.com>
2023-01-18 18:26:21 +0200

    Stylistic changes; moved to flex

--- src/App.js
+++ src/App.js
@@ -2,14 +2,39 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import getOffenders from './services/offenders';
 
-import OffenderTable from './components/OffenderTable'
+import OffenderTable from './components/OffenderTable';
+import { VictoryChart, VictoryAxis, VictoryScatter, VictoryTheme } from 'victory';
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [posData, setPosData] = useState([])
+
+
+  const getPosData = (data) => {
+    const newPos = [
+      {
+        x: 250,
+        y: 250
+      }];
+
+    data.map(datum => {
+      newPos.push(
+        {
+          x: datum.x / 1000,
+          y: datum.y / 1000
+        }
+      )
+    });
+
+    setPosData(newPos);
+  }
 
   useEffect(() => {
     const interval = setInterval(
-      () => getOffenders().then(data => setData(data)),
+      () => getOffenders().then(res => {
+        setData(res);
+        getPosData(res);
+      }),
       2000);
 
     return () => {
@@ -19,9 +44,51 @@ const App = () => {
 
 
   return (
-    <>
-      <OffenderTable offenders={ data } />
-    </>
+    <div className='main'>
+      
+      <div className='graph'>
+
+        <VictoryChart
+        theme={VictoryTheme.material}
+        domain={{
+          x: [150, 350],
+          y: [150, 350] 
+        }}>
+          <VictoryAxis 
+              dependentAxis
+              style={{
+                grid: { stroke: "none" },
+                axis: {stroke: "transparent"}, 
+                ticks: {stroke: "transparent"},
+                tickLabels: { fill:"transparent"} 
+              }}
+            />
+            <VictoryAxis 
+              independentAxis
+              style={{
+                grid: { stroke: "none" },
+                axis: {stroke: "transparent"}, 
+                ticks: {stroke: "transparent"},
+                tickLabels: { fill:"transparent"} 
+              }}
+            />
+          <VictoryScatter
+            style={ {
+              data: { fill: ({datum}) => datum.x === 250 && datum.y === 250 ? "#dea176" : "#f22d54" },
+              grid: { stroke: "none" }
+              } }
+            size={ 4 }
+            data={ posData }
+          />
+
+        </VictoryChart>
+
+      </div>
+
+      <div className='tableContainer'>
+        <OffenderTable offenders={ data } />
+      </div>
+    </div>
   );
 }
 
