c40b733854703566dbacfddcddfb464e53d5f0cc
Ron Kosova <ron.kosova@gmail.com>
2023-01-18 16:48:03 +0200

    Table styling

--- src/components/OffenderTable.js
+++ src/components/OffenderTable.js
@@ -8,27 +8,27 @@ const OffenderTable = ({ offenders }) => {
     return (
         <table className='offenderTable'>
             <thead>
-            <tr>
-                <th></th>
-                <th>Snapshot</th>
-                <th>Distance</th>
-                <th>Pilot ID</th>
-                <th>Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Phone Number</th>
-            </tr>  
+                <tr>
+                    <th></th>
+                    <th>Snapshot</th>
+                    <th>Distance</th>
+                    <th>Pilot ID</th>
+                    <th>Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                </tr>  
             </thead>
             <tbody>
-            { offenders.map((offender, i) => {
-                return (
-                <OffenderRow 
-                    offender={ offender } 
-                    index = { i } 
-                    key={ offender.id }
-                />
-                );
-            }) }
+                { offenders.map((offender, i) => {
+                    return (
+                    <OffenderRow 
+                        offender={ offender } 
+                        index = { i } 
+                        key={ offender.id }
+                    />
+                    );
+                }) }
             </tbody>
       </table>
     );
